57136c7866814f8375c25e437ce8bdd195e9e837
ryym <ryym.64@gmail.com>
2023-11-19 19:32:06 +0900

    feat: Read and print inputs and quit on Ctrl-q

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,29 @@
-use std::io::{self, Read};
+use std::io::{self, Read, Write};
+use termion::raw::IntoRawMode;
 
 fn main() {
-    println!("Hello, world!");
+    let mut stdout = io::stdout().into_raw_mode().unwrap();
 
     for b in io::stdin().bytes() {
-        let c = b.unwrap() as char;
-        print!("{}", c);
-        if c == 'q' {
-            break;
+        match b {
+            Ok(b) => {
+                let c = b as char;
+                if c.is_control() {
+                    println!("{:?} {:#b} \r", b, b)
+                } else {
+                    println!("{:?} {:#b} ({})\r", b, b, c)
+                }
+                stdout.flush().unwrap();
+                if b == to_ctrl_byte('q') {
+                    break;
+                }
+            }
+            Err(e) => panic!("{}", e),
         }
     }
+}
+
+fn to_ctrl_byte(c: char) -> u8 {
+    let byte = c as u8;
+    byte & 0b001_1111
 }
