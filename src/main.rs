f5d062734f55ec3ffc30eccbcd1a6955cfb6532e
ryym <ryym.64@gmail.com>
2023-11-19 13:13:31 +0900

    feat: Print each char and quit with "q"

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io::{self, Read};
+
 fn main() {
     println!("Hello, world!");
+
+    for b in io::stdin().bytes() {
+        let c = b.unwrap() as char;
+        print!("{}", c);
+        if c == 'q' {
+            break;
+        }
+    }
 }
